7c9e207af0b65a24a38f5f465e70cb2c2fd59e4a
BassamSaleh99 <bassam.saleh.1002@gmail.com>
2024-11-20 12:13:37 -0800

    Interpolate Ball Positions

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import supervision as sv
 import numpy as np
+import pandas as pd
 import pickle
 import cv2
 import os
@@ -12,6 +13,18 @@ class Tracker:
     def __init__(self, model_path):
         self.model = YOLO(model_path)
         self.tracker = sv.ByteTrack()
+
+    def interpolate_ball_positions(self, ball_positions):
+        ball_positions = [x.get(1, {}).get('bbox', []) for x in ball_positions]
+        df_ball_positions = pd.DataFrame(ball_positions, columns=['x1', 'y1', 'x2', 'y2'])
+
+        # Interpolate missing Values
+        df_ball_positions = df_ball_positions.interpolate()
+        df_ball_positions = df_ball_positions.bfill()
+
+        ball_positions = [{1: {'bbox':x}} for x in df_ball_positions.to_numpy().tolist()]
+
+        return ball_positions
 
     def detect_frames(self, frames):
         batch_size = 20
@@ -142,7 +155,6 @@ class Tracker:
 
         return frame
 
-
     def draw_annotations(self, video_frames, tracks):
         output_video_frames = []
 
@@ -170,3 +182,4 @@ class Tracker:
             output_video_frames.append(frame)
 
         return output_video_frames
+    
